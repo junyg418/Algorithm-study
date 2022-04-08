d90435c1ebc759684fa08ef9e2eee6aa1bcf9459
KIMYO9 <fgh33678@gmail.com>
2022-04-08 14:19:13 +0900

    bookal-92p 30:20 +설명

--- bookal-92p.py
+++ bookal-92p.py
@@ -1,6 +1,10 @@
 '''
 알고리즘 책 92P
 큰 수의 법칙 난이도 하
+1. 1회 오답, 수정후 정답(30:00)
+-주석
+    한번 k번 사용된 인덱스는 사용이 안된다 이해함
+    즉 문제 잘 안읽음
 '''
 
 n, m, k = map(int, input().split())
@@ -8,6 +12,7 @@ li = list(map(int, input().split()))
 li.sort(reverse=True)
 cnt = 0
 circle = 0
+
 # print(li)
 # for num in range(n): 
 #     for _ in range(k):
