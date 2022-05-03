3c3a5320d22043b5c1fe085b464dcf215628edc8
KIMYO9 <fgh33678@gmail.com>
2022-05-03 13:35:19 +0900

    2022-05-03-197p

--- bookal/Chapter 07/bookal-197p.py
+++ bookal/Chapter 07/bookal-197p.py
@@ -4,29 +4,53 @@ Chapter 07 이진 탐색
 알고리즘 책 197p
 부품 찾기|난이도 중하|풀이시간 30분|
 1. 10분컷 유후~~
+2022-05-03-09:12
+2. 3분컷
 '''
-import sys
-sys.stdin = open('input')
-sys.setrecursionlimit(10**8)
-n = int(input())  # 전체 물건 개수
-array = list(map(int, sys.stdin.readline().split()))  #부품의 고유번호들
-
-m = int(input()) # 필요 부품 개수
-targets = list(map(int, sys.stdin.readline().split())) # 찾고자 하는 물품의 번호
-
-def binary_serach(array, target, start, end):
-    if start > end:
+def binary_search(array, target, start, end):
+    if start> end:
         return None
     mid = (start+end)//2
     if array[mid] == target:
         return mid
     elif array[mid] > target:
-        return binary_serach(array, target, 0, mid-1)
+        return binary_search(array, target, 0, mid-1)
     else:
-        return binary_serach(array, target, mid+1, end)
+        return binary_search(array, target, mid+1, end)
+n = int(input())
+array = list(map(int, input().split()))
+m = int(input())
+targets = list(map(int, input().split()))
 
 for target in targets:
-    if binary_serach(array, target, 0, n-1) == None:
+    if binary_search(array, target, 0, n-1) == None:
         print('no', end=' ')
     else:
         print('yes', end=' ')
+
+
+# import sys
+# sys.stdin = open('input')
+# sys.setrecursionlimit(10**8)
+# n = int(input())  # 전체 물건 개수
+# array = list(map(int, sys.stdin.readline().split()))  #부품의 고유번호들
+
+# m = int(input()) # 필요 부품 개수
+# targets = list(map(int, sys.stdin.readline().split())) # 찾고자 하는 물품의 번호
+
+# def binary_serach(array, target, start, end):
+#     if start > end:
+#         return None
+#     mid = (start+end)//2
+#     if array[mid] == target:
+#         return mid
+#     elif array[mid] > target:
+#         return binary_serach(array, target, 0, mid-1)
+#     else:
+#         return binary_serach(array, target, mid+1, end)
+
+# for target in targets:
+#     if binary_serach(array, target, 0, n-1) == None:
+#         print('no', end=' ')
+#     else:
+#         print('yes', end=' ')
