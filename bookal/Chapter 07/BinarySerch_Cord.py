ed91b1583d1a0b9c5b970a0afe0b1a3c213cc214
KIMYO9 <fgh33678@gmail.com>
2022-05-12 13:27:29 +0900

    2022-05-12 복습 정답

--- bookal/Chapter 07/BinarySerch_Cord.py
+++ bookal/Chapter 07/BinarySerch_Cord.py
@@ -1,6 +1,8 @@
 #재귀합수로 구현한 이진 탐색코드
-
-def binary_serch(array:list, target:int, start, end):
+'''
+2022-05-12 복습 정답완료
+'''
+def binary_search(array:list, target:int, start, end):
     '''
     array:배열
     target:찾으려고 하는 원소
@@ -15,17 +17,17 @@ def binary_serch(array:list, target:int, start, end):
         return target
     # 중간점의 값보다 찾고자 하는 값이 작은 경우->왼쪽 확인
     elif array[mid] > target:
-        return binary_serch(array, target, start, mid-1)
+        return binary_search(array, target, start, mid-1)
     # 중간점의 값보다 찾고자 하는 값이 큰 경우->오른쪽 확인
     else:
-        return binary_serch(array, target, mid+1, end)
+        return binary_search(array, target, mid+1, end)
 
 # n(원소의 개수)과 target(찾고자 하는 문자열)
 n, target = map(int, input().split())
 # 전체 원소 배열
 array = list(map(int, input().split()))
 
-result = binary_serch(array, target, 0, n-1)
+result = binary_search(array, target, 0, n-1)
 if result == None:
     print('결과가 존재하지 않습니다.')
 else:
