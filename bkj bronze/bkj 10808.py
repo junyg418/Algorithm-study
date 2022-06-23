a809db7d667d8ac32c4f20529adb6721291ab7b0
KIMYO9 <fgh33678@gmail.com>
2022-06-23 16:57:03 +0900

    2022-06-23

---
+++ bkj bronze/bkj 10808.py
@@ -0,0 +1,24 @@
+'''
+알파벳 개수(10808)b4
+2022-06-23
+1.10분컷
+'''
+
+# dic = {}
+# for _ in range(36):
+#     a = input()
+#     dic[a] = 0
+# print(dic)
+#a b c d e ~ z를 입력하여 딕셔너리 쉽게 만듦
+#-------------------
+
+s = input()
+dic = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 
+'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 
+'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+
+for string in s:
+    dic[string] += 1
+
+for num in dic.values():
+    print(num, end=' ')
