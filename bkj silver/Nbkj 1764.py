cba8d04530074d617871ff268b6c98c3b62ad5d0
KIMYO9 <fgh33678@gmail.com>
2022-06-30 23:33:41 +0900

    2022-06-30 Fail

---
+++ bkj silver/Nbkj 1764.py
@@ -0,0 +1,47 @@
+'''
+듣보잡(1764)S4
+2022-06-30
+1.시간초과
+2.틀렸습니다.
+3.틀렸습니다.
+'''
+# from sys import stdin
+
+# n, m = map(int, input().split())
+# donthear = []
+# answer = []
+# for _ in range(n):
+#     donthear.append(stdin.readline().strip())
+# for _ in range(m):
+#     x=stdin.readline().strip()
+#     if x in donthear:
+#         answer.append(x)
+# print(len(answer))
+# for i in answer:
+#     print(i)
+
+
+##### Case2 #####
+# from sys import stdin
+
+# n, m = map(int, input().split())
+# # f = open('input')
+# a = stdin.readlines()
+# see, look = a[:n], a[n:]
+# answer = list(set(see).intersection(look))
+# print(len(answer))
+# for _ in answer:
+#     print(_.strip())
+
+
+##### Case3 #####
+# from sys import stdin
+
+# n, m = map(int, input().split())
+# # f = open('input')
+# a = list(map(str.strip, stdin.readlines()))
+# see, look = a[:n], a[n:]
+# answer = list(set(see).intersection(look))
+# print(len(answer))
+# for _ in answer:
+#     print(_)
