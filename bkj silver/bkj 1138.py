d1f22689f4744ac58f770b8ada8f59558291e80d
KIMYO9 <fgh33678@gmail.com>
2022-07-26 00:27:01 +0900

    2022-07-26

--- bkj silver/bkj 1138.py
+++ bkj silver/bkj 1138.py
@@ -1,25 +1,26 @@
 '''
 한 줄로 서기(1138)s2
 2022-07-25
-1.
+2022-07-26
+1.예제 성공 but RuntimError
+-사유
+    join 사용시 answer들의 요소가 int였기에 오류
+2.정답
 '''
-from turtle import pos
-
-
 man_count = int(input())
 hight_info_list = list(map(int, input().split()))
 
-success_idx_list = [0 for _ in range(man_count)]
+success_idx_list = [None for _ in range(man_count)]
 
 def zero_counter(value):
-    pos = 1
+    pos = 0
     while True:
-        if answer[:pos].count(0) >= value:
+        if answer[:pos].count(None) >= value:
             return pos
         pos +=1
 
 
-answer = [0 for _ in range(man_count)]
+answer = [None for _ in range(man_count)]
 for count, count_num in enumerate(hight_info_list):
     success = True
     pos = zero_counter(count_num)
@@ -27,10 +28,11 @@ for count, count_num in enumerate(hight_info_list):
     while success:
         if pos not in success_idx_list:
             answer[pos] = count+1
-            success_idx_list[count] = count_num
+            success_idx_list[count] = pos
 
             success = False
                 
         else:
             pos+=1
-print(success_idx_list)
+# print(' '.join(answer)
+print(' '.join(map(str, answer)))
