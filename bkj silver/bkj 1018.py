6a3430aa6bb8bb01fa7de66d8e1bd3c1cc5912d2
Jun Young Lee <fgh33678@gmail.com>
2023-07-03 21:48:05 +0900

    sliver 문제 해결!

---
+++ bkj silver/bkj 1018.py
@@ -0,0 +1,39 @@
+row, column = map(int, input().split())
+
+ex1 = [["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"]]
+
+ex2 = [["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"],
+       ["B","W","B","W","B","W","B","W"],
+       ["W","B","W","B","W","B","W","B"]]
+
+input_value = [input() for _ in range(row)]
+
+def getCount(start_pos:tuple, sample:list)->int:
+    count = 0
+    ro, co = start_pos
+    for r in range(8):
+        for c in range(8):
+            if input_value[ro+r][co+c] == sample[r][c]:
+                count += 1
+    return count
+
+
+result_v = []
+
+for rr in range(row-7):
+    for cc in range(column-7):
+       result_v.append(min(getCount((rr, cc), ex1), getCount((rr, cc), ex2)))
+
+print(min(result_v))
