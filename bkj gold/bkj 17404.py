2e93e79cf14f7e26177f3a9166bae807fd3afd96
이준영 <fgh33678@gmail.com>
2024-02-27 16:08:09 +0900

    파일 정리 및 경로 변경

--- bkj gold/bkj 17404.py
+++ bkj gold/bkj 17404.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data:int, idx:int) -> None:
+    def __init__(self, data:int, idx:int):
         self.data = data # 숫자값
         self.idx = idx   # 색상 0, 1, 2
 
@@ -13,15 +13,18 @@ for idx, data in enumerate(temp):
     result_list[idx].append(Node(data, idx))
 
 level = 1
+
 # 전체 반복
 for _ in range(t-1):
     temp = list(map(int, input().split()))
 
     # 3개의 완전 이진트리에 값 추가
     for node_idx in range(3):
+        current_node_cnt = len(result_list[node_idx])
         # 각 데이터 노드에 데이터 추가
-        for line in range(2**level):
-            parent_node:Node = result_list[node_idx][2**level+line]
+        for line in range(2**(level-1)):
+            parent_node:Node = result_list[node_idx][2**(level-1)+line]
+
 
             # 자식 추가
             for idx, data in enumerate(temp):
@@ -30,3 +33,15 @@ for _ in range(t-1):
                 result_list[node_idx].append(Node(parent_node.data+data, idx))
 
     level += 1
+
+fin_list = []
+for tree_cnt in range(3):
+    while True:
+        min_data:Node = min(result_list[tree_cnt][2**(level-1):], key=lambda x: x.data)
+        if min_data.idx == tree_cnt:
+            result_list[tree_cnt].remove(min_data)
+            continue
+        break
+    fin_list.append(min_data)
+
+print(min(fin_list, key=lambda x:x.data).data)
--- test.py
+++ test.py
@@ -1,20 +1,5 @@
-
-# # # # a={(1,2):"오늘은", (2,2):"내일이다"}
-# # # # print(a[(2,2)])
-# # # for i in range(10):
-# # #     globals()[f"num{i}"] ="오렌지"
-# # a = [1,2,2,3]
-# # a.remove(2)
-# # print(a)
-
-# print(repr("""안녕
-# 하시오"""))
-# class AF:
-#     pass
-# aa = ["문d자열", "호롤로"]
-# a = "answ"
-# print(type(None) == None)'
-
-lt = [1,2,3,4]
-
-for i in lt:
+n=5
+a= [3] * n
+print(a)
+a[1]=2
+print(a)
